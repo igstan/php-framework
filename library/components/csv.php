1741c8d50c09f90f22ac336b328f9923b08e888f
Ionut G. Stan <ionut.g.stan@gmail.com>
2009-03-03 12:57:56 +0200

    Added a default implementation for a CSV reader

--- library/components/csv.php
+++ library/components/csv.php
@@ -3,11 +3,110 @@
 interface igs_CsvReader extends ArrayAccess, Iterator
 {
     /**
-     * @param string $csvFile
-     * @param array  $options OPTIONAL
+     * @param string|igs_Stream $csvFile
+     * @param array|ArrayAccess $options OPTIONAL
      */
-    public function __construct($csvFile, array $options = array());
+    public function __construct($csvFile, $options = array());
 }
 
 interface igs_CsvWriter
-{}
+{
+    /**
+     * Options may include any or all of the following keys:
+     *      - delimiter, defaults to ,(comma)
+     *      - enclosure, defaults to "(double quote)
+     *      - escape,    defaults to \(backslash)
+     *
+     * @param array|Traversable|igs_Stream $stream
+     * @param array|ArrayAccess $options
+     */
+    public function __construct($data, $options = array());
+}
+
+class igs_DefaultCsvReader implements igs_CsvReader
+{
+    /**
+     * @var string Character that separates cell values
+     */
+    protected $delimiter = ',';
+
+    /**
+     * @var string Character that encloses the value of each "cell"
+     */
+    protected $enclosure = '"';
+
+    /**
+     * @var string Caracter to escape special characters
+     */
+    protected $escape    = '\\';
+
+    /**
+     * Options may include any or all of the following keys:
+     *      - delimiter, defaults to ,(comma)
+     *      - enclosure, defaults to "(double quote)
+     *      - escape,    defaults to \(backslash)
+     *
+     * @param  array|Traversable|igs_Stream $stream
+     * @param  array|ArrayAccess $options
+     */
+    public function __construct($data, $options = array())
+    {
+        $this->setStream($data);
+        $this->setOptions($options);
+    }
+
+    /**
+     * @param  array|Traversable|igs_Stream $stream
+     * @return igs_DefaultCsvReader
+     */
+    protected function setStream($data)
+    {
+        if (! $data instanceof igs_Collection) {
+            $data = igs_DefaultCollectionFactory($data);
+        }
+
+        $this->stream = $data;
+        return $this;
+    }
+
+    protected function setOptions($options)
+    {
+        if (! $options instanceof igs_Collection) {
+            $option = igs_DefaultFactoryCollection($data);
+        }
+
+        $option->each(array($this, 'setOption'));
+    }
+
+    /**
+     * @param string $value
+     * @param string $name  OPTIONAL
+     */
+    protected function setOption($value, $name = null)
+    {
+        $options = array('delimiter', 'enclosure', 'escape');
+
+        if (in_array($name, $options)) {
+            $this->$name = $value;
+        }
+
+        return $this;
+    }
+}
+
+/**
+ * Options may include any or all of the following keys:
+ *      - delimiter, defaults to ,(comma)
+ *      - enclosure, defaults to "(double quote)
+ *      - escape,    defaults to \(backslash)
+ *
+ * @param  array|Traversable|igs_Stream $stream
+ * @param  array|ArrayAccess $options
+ * @return igs_DefaultCsvReader
+ */
+function igs_DefaultCsvReader($data, $options = array())
+{
+    return new igs_DefaultCsvReader($data, $options);
+}
+
+define('igs_DefaultCsvReader', 'igs_DefaultCsvReader');
