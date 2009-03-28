d41a3cbe52965f08aad207a520754ded42b67684
Ionut G. Stan <ionut.g.stan@gmail.com>
2009-03-28 02:05:44 +0200

    Copied functions from igs\utils in a new package igs, which should be framework's global namespace

---
+++ library/components/igs.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace igs
+{
+    /**
+     * @param  mixed $iter
+     * @return boolean
+     */
+    function is_iter($iter)
+    {
+        return is_array($iter) || $iter instanceof \Traversable;
+    }
+
+    /**
+     * @param  mixed $iter
+     * @return boolean
+     */
+    function is_hashtable($table)
+    {
+        return is_array($table) || $table instanceof \ArrayAccess;
+    }
+
+    function call()
+    {
+        $args = func_get_args();
+        $call = array_unshift($args);
+
+        if (! is_callable($call))
+        {
+            throw new \InvalidArgumentException(
+                'First argument must be a callable. ' . gettype($call) . ' given'
+            );
+        }
+
+        return call_user_func_array($call, $args);
+    }
+}
